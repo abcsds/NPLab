8c2ba4f8be5212f17ac80379a349ac51e9b78b8c
Alberto Barradas <abcsds@gmail.com>
2020-09-18 11:32:15 +0200

    Remove heartpy in favor of biosppy
    Update exploration in turn

--- analysis/exploration.py
+++ analysis/exploration.py
@@ -2,26 +2,25 @@ import pyxdf
 import matplotlib.pyplot as plt
 import numpy as np
 
-data, header = pyxdf.load_xdf('analysis/raw/BPresp.xdf')
-markers = {}
-plt.figure(figsize=(20, 7))
-for stream in data:
-    y = stream['time_series']
-    if isinstance(y, list):
-        # list of strings, draw one vertical line for each marker
-        for timestamp, marker in zip(stream["time_stamps"], y):
-            plt.axvline(x=timestamp, c="red")
-            print(f'Marker "{marker[0]}" @ {timestamp:.2f}s')
-            markers[marker[0]] = timestamp
-    elif isinstance(y, np.ndarray):
-        # numeric data, draw as lines
-        fs = int(stream["info"]["nominal_srate"][0])
-        plt.plot(stream["time_stamps"], y)
-        ecg, resp, gsr = np.split(y, [1,2], axis=1)
-        t = stream["time_stamps"]
-    else:
-        raise RuntimeError('Unknown stream format')
 
+data, header = pyxdf.load_xdf('analysis/raw/000.xdf')
+
+plt.figure(figsize=(16, 7))
+markers = {}
+# with data[0] as stream: # RDA external electrodes
+stream = data[0]
+y = stream['time_series']
+# numeric data, draw as lines
+fs = int(stream["info"]["nominal_srate"][0])
+plt.plot(stream["time_stamps"], y)
+ecg, resp, gsr = np.split(y, [1,2], axis=1)
+t = stream["time_stamps"]
+# with data[2] as stream:
+stream = data[2]
+for marker, ts in zip(stream["time_series"], stream["time_stamps"]):
+    plt.axvline(x=ts, c="red")
+    markers[marker[0]] = ts
+plt.show()
 
 # signal processing
 from scipy import signal
@@ -38,39 +37,53 @@ def bandpass_filter(data, lowcut, highcut, fs, order=4):
     y = signal.lfilter(b, a, data)
     return y
 
-def notch_filter(data, fs, f0=50, Q=2):
+def notch_filter(data, fs, f0=50, Q=30):
     b, a = signal.iirnotch(f0, Q, fs)
     y = signal.lfilter(b, a, data)
     return y
 
-filtered_sig = notch_filter(butter_bandpass(ecg, 0.01, .1, fs), fs)
-filtered_sig = ecg
+def detrend_filter(data):
+    return data - data.mean()
+
+non_filtered = detrend_filter(-ecg)
+# non_filtered = -ecg
+filtered_sig = bandpass_filter(non_filtered, 0.01, 45, fs)
+filtered_sig = notch_filter(filtered_sig, fs)
 
 samples = 6000
 plt.figure(figsize=(16, 7))
 plt.title("ECG signal")
-plt.plot(t[0:samples], filtered_sig[0:samples], color="#51A6D8", linewidth=1)
+plt.plot(t[0:samples], filtered_sig[0:samples], color="#51d79b", linewidth=1)
+plt.plot(t[0:samples], non_filtered[0:samples], color="#51A6D8", linewidth=1)
 plt.xlabel("Time (s)", fontsize=16)
-plt.ylabel("Amplitude (arbitrary unit)")
+plt.ylabel("Amplitude (uV)")
 plt.show()
 
+from biosppy.signals import ecg
 
-import heartpy as hp
+# load raw ECG signal
+signal = filtered_sig.flatten()
 
-hrdata = ecg.flatten()
+# process it and plot
+out = ecg.ecg(signal=signal, sampling_rate=fs, show=True)
 
-filtered = hp.filter_signal(hrdata,
-                            cutoff=[0.75, 3.5],
-                            sample_rate=fs,
-                            order=3,
-                            filtertype='bandpass')
+plt.figure(figsize=(50, 7))
+plt.plot(out["ts"], out["filtered"],
+         color="#51A6D8",
+         linewidth=1)
+plt.scatter(out["ts"][out["rpeaks"]],
+            out["filtered"][out["rpeaks"]],
+            color="#df4040",
+            linewidth=1)
 
-filtered = hp.filter_signal(filtered,
-                            cutoff=1,
-                            sample_rate=fs,
-                            filtertype='notch')
 
-working_data, measures = hp.process(hrdata, fs)
-plt.figure(figsize=(150, 7))
-plt.title("ECG signal")
-hp.plotter(working_data, measures)
+plt.figure(figsize=(16, 7))
+for y in out["templates"]:
+    plt.plot(out["templates_ts"], y, color="#19608a", linewidth=1)
+
+
+plt.figure(figsize=(16, 7))
+plt.plot(out["heart_rate_ts"], out["heart_rate"],
+         color="#51A6D8",
+         linewidth=1)
+# TODO: RRintervals, hrv, breath amplitude, gsr mean, segmentation.
