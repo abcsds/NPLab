f4e48e6a96f5069a96576adc7728823e54278572
Alberto Barradas <abcsds@gmail.com>
2020-09-21 10:27:36 +0200

    Add SDNN for hrv

--- analysis/exploration.py
+++ analysis/exploration.py
@@ -62,7 +62,7 @@ plt.show()
 from biosppy.signals import ecg
 
 # load raw ECG signal
-signal = filtered_sig.flatten()
+signal = non_filtered.flatten()
 
 # process it and plot
 out = ecg.ecg(signal=signal, sampling_rate=fs, show=True)
@@ -76,10 +76,9 @@ plt.scatter(out["ts"][out["rpeaks"]],
             color="#df4040",
             linewidth=1)
 
-
 plt.figure(figsize=(16, 7))
 for y in out["templates"]:
-    plt.plot(out["templates_ts"], y, color="#19608a", linewidth=1)
+    plt.plot(out["templates_ts"], y, color="#51A6D8", linewidth=1, alpha=0.2)
 
 
 plt.figure(figsize=(50, 7))
@@ -89,12 +88,38 @@ plt.plot(out["heart_rate_ts"], out["heart_rate"],
 
 # R-R intervals
 rr_intervals = np.diff(out["ts"][out["rpeaks"]], prepend=[np.nan])
+rr_intervals *= 1000 # Convert from s to ms
+
+stream = data[2]
 plt.figure(figsize=(16, 7))
 plt.plot(out["ts"][out["rpeaks"]] + stream["time_stamps"].min(),
          rr_intervals,
          linewidth=1)
 for marker, ts in zip(stream["time_series"], stream["time_stamps"]):
     plt.axvline(x=ts, c="red")
-    markers[marker[0]] = ts
+plt.xlabel("Time (s)")
+plt.ylabel("RR-interval (ms)")
 
-# TODO: hrv, segmentation (remember time delay), breath amplitude, gsr mean.
+# Rolling window for SDNN
+def rolling_std(vec, window_size):
+    shape = vec.shape[:-1] + (vec.shape[-1] - window_size + 1, window_size)
+    strides = vec.strides + (vec.strides[-1],)
+    windows = np.lib.stride_tricks.as_strided(vec, shape=shape, strides=strides)
+    rstd = np.hstack((np.zeros(window_size // 2) + np.nan,
+                      np.std(windows, 1),
+                      np.zeros((window_size // 2) - 1) + np.nan))
+    return rstd
+
+n = 8 # Must be even number for now
+hrv = rolling_std(rr_intervals, n)
+plt.figure(figsize=(16, 7))
+plt.plot(out["ts"][out["rpeaks"]] + stream["time_stamps"].min(),
+         hrv,
+         color="#51A6D8",
+         linewidth=1)
+for marker, ts in zip(stream["time_series"], stream["time_stamps"]):
+    plt.axvline(x=ts, c="red")
+plt.xlabel("Time (s)")
+plt.ylabel("SDNN (ms)")
+
+# TODO: segmentation (remember time delay), breath amplitude, gsr mean.
